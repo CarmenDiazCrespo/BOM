6b23258cb9fd095b956ac8b0cd0d400ac7a94739
CarmenDiazCrespo <32709202+CarmenDiazCrespo@users.noreply.github.com>
2019-02-17 20:06:48 +0100

    versión 1.0.0

---
+++ js/BOM.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var cont = 0;
+var ventana = [];
+function abrirVentana(production){
+    cont++;
+	var pagina= "NuevaPestaña.html";
+	var main = document.getElementById("div-main");
+	
+	var h2 = document.createElement("h2");
+    h2.appendChild(document.createTextNode(production));
+	main.appendChild(h2);
+	
+	ventana[cont]=window.open(pagina);
+}
+
+function cerrarVentanas(){
+	for(var i = 1; i <= cont; i++){
+		ventana[i].close();
+	}
+	cont=0;
+}
+
